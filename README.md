ec25e3242de012b2562c3b99a68229a405822aa9
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-02-12 10:22:51 +0100

    :tada: feat: new docs

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # SnailyCADv4 Docs
 
 The docs for SnailyCADv4
-
-## Template
-
-This site is created with [Nextra](https://nextra.vercel.app)
---
+++ docusaurus.config.js
@@ -0,0 +1,58 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "SnailyCADv4 Documentation",
+  tagline: "Dinosaurs are cool",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "log",
+  onBrokenMarkdownLinks: "warn",
+  organizationName: "SnailyCAD",
+  projectName: "docs",
+
+  presets: [
+    [
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      }),
+    ],
+  ],
+
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
+  themeConfig: {
+    navbar: {
+      title: "SnailyCADv4 Docs",
+      items: [
+        {
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Documentation",
+        },
+        {
+          href: "https://github.com/SnailyCAD/snaily-cadv4",
+          label: "GitHub",
+          position: "right",
+        },
+      ],
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+};
+
+module.exports = config;
