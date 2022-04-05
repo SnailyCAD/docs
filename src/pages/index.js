8d29d1c0de318f2b1ed69efe7796fa84e2e42d80
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-04-05 09:10:56 +0200

    :hammer: chore: minor refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Layout from "@theme/Layout";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Head from "@docusaurus/Head";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -9,5 +10,16 @@ export default function Home() {
     window.location.href = "/docs/intro";
   });
 
-  return <Layout title={siteConfig.title} description="Documentation provided for SnailyCADv4" />;
+  return (
+    <Layout title={siteConfig.title} description="Documentation provided for SnailyCADv4">
+      <Head>
+        <script
+          async
+          defer
+          data-website-id="5d735bf8-9086-43df-9da8-62c2e49401d5"
+          src="https://umami-nine-olive.vercel.app/umami.js"
+        />
+      </Head>
+    </Layout>
+  );
 }
