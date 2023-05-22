a1c31b54ca6499dac59537b21f75898bfb63edf4
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-05-22 07:02:21 +0200

    chore: bump deps

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer/dist");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -157,8 +156,8 @@ const config = {
       ],
     },
     prism: {
-      theme: lightCodeTheme,
-      darkTheme: darkCodeTheme,
+      theme: themes.github,
+      darkTheme: themes.dracula,
       additionalLanguages: ["lua"],
     },
   },
