e6c73b318c231df57fb3c145e669fb78c8b667c1
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-08-17 08:36:52 +0200

    feat: 1.67.0

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,12 +14,6 @@ export default function Home() {
     <Layout title={siteConfig.title} description="Documentation provided for SnailyCADv4">
       <Head>
         <link rel="icon" href="https://docs.snailycad.org/icons/logo.png" />
-        <script
-          async
-          defer
-          data-website-id="5d735bf8-9086-43df-9da8-62c2e49401d5"
-          src="https://cool-kid.caspertheghost.me/umami.js"
-        />
       </Head>
     </Layout>
   );
